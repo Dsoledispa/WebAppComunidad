e5aa74ff9128c0237102bca05ab8536ecc315e94
Diego <diegojavi2026@gmail.com>
2021-12-02 17:20:59 +0100

    Errores 2

--- view/menu.php
+++ view/menu.php
@@ -5,12 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Documento</title>
 </head>
 
 <body>
     <button type="submit"><a type='button' href='login.html'>Acceder al login</a></button>
     <button type="submit"><a type='button' href='voluntarios.php'>Acceder al formulario</a></button>
+<br><br><br>
+<div class="row">
+    <div class="three-column" ><a href="./voluntarios.php"><img src="../img/alimentos.png"></a></div>
+    <div class="three-column"><a href="./voluntarios.php"><img src="../img/carrera.png"></a></div>
+    <div class="three-column"><a href="./voluntarios.php"><img src="../img/sangre.png"></a></div>
+</div>
+
+
+
+
 </body>
 
-</html>
+</html>  
