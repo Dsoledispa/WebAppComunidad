03617ba8ee7d850b48fabde9fae1416cb81742d1
Jacko <100005774.joan23@fje.com>
2021-12-02 19:48:15 +0100

    ESTILOS

--- view/formularioeventos.php
+++ view/formularioeventos.php
@@ -2,7 +2,7 @@
 require_once "../services/connection.php";
 session_start();
 if ($_SESSION['email']=="") {
-    header("location:login.html");
+    header("location:menu.php");
 }else {
 ?>
 <!DOCTYPE html>
